551a81fc1d528f934ef9e881baeca4265e1ba6c1
longghoang <nguyenlonglqmb@gmail.com>
2024-08-03 20:41:52 +0700

    OK

--- src/app/utils/errorFlow.util.js
+++ src/app/utils/errorFlow.util.js
@@ -3,8 +3,7 @@ const createError = require('http-errors');
 module.exports = function errorFlow(error, next) {
     if (error.response) {
         console.log("Error status: ", error.response.status);
-        console.log("Error status: ", error.data.message);
-        next(createError(error.response.status, error.response.data.message));
+        next(createError(error.response.status, "Error"));
     } else {
         console.log('Error!');
         next(createError(500, 'Internal server error'));
