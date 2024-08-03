82b53e1465282bc32020ce0f64836bf57fbaf315
longghoang <nguyenlonglqmb@gmail.com>
2024-08-03 20:47:49 +0700

    Error

--- src/app/controllers/Auth.js
+++ src/app/controllers/Auth.js
@@ -107,18 +107,19 @@ class Auth {
 
     async login(req, res, next) {
         const userData = req.body;
-        if(!userData.email || !userData.password) return res.status(400).json({ message: 'Invalid request data' });
-        try {
-            const response = await axios.post(`${process.env.AUTH_SERVER}/login`, userData);
-            const {uid, accessToken} = response.data;
-            const cookieOptions = { signed: true, maxAge: 3600000 };
-            return res.cookie('act', accessToken, cookieOptions)
-            .cookie('uid', uid, cookieOptions)
-            .redirect('/');
-        } catch (error) {
-            errorFlow(error, next);
+        return res.send(userData);
+        // if(!userData.email || !userData.password) return res.status(400).json({ message: 'Invalid request data' });
+        // try {
+        //     const response = await axios.post(`${process.env.AUTH_SERVER}/login`, userData);
+        //     const {uid, accessToken} = response.data;
+        //     const cookieOptions = { signed: true, maxAge: 3600000 };
+        //     return res.cookie('act', accessToken, cookieOptions)
+        //     .cookie('uid', uid, cookieOptions)
+        //     .redirect('/');
+        // } catch (error) {
+        //     errorFlow(error, next);
 
-        }
+        // }
     }
     async logout(req, res, next) {
         const act = req.signedCookies.act;
--- src/app/utils/errorFlow.util.js
+++ src/app/utils/errorFlow.util.js
@@ -3,7 +3,8 @@ const createError = require('http-errors');
 module.exports = function errorFlow(error, next) {
     if (error.response) {
         console.log("Error status: ", error.response.status);
-        next(createError(error.response.status, "Error"));
+        console.log("Error status: ", error.data.message);
+        next(createError(error.response.status, error.response.data.message));
     } else {
         console.log('Error!');
         next(createError(500, 'Internal server error'));
