6c1e1e0f46dd06fc018141ef1f71c862a89bd146
longghoang <nguyenlonglqmb@gmail.com>
2024-08-04 23:50:01 +0700

    ok

--- src/app/utils/errorFlow.util.js
+++ src/app/utils/errorFlow.util.js
@@ -3,23 +3,8 @@ const createError = require('http-errors');
 module.exports = function errorFlow(error, next) {
     if (error.response) {
         console.log("Error status: ", error.response.status);
-        console.log("Error response data: ", error.response.data);
-
-        let errorMessage;
-        
-        if (typeof error.response.data === 'string' && error.response.data.includes('<html')) {
-            // If the response data is HTML, provide a generic error message
-            errorMessage = 'Received HTML error response from server';
-        } else if (error.response.data && error.response.data.message) {
-            errorMessage = error.response.data.message;
-        } else if (error.response.data && error.response.data.error) {
-            errorMessage = error.response.data.error;
-        } else {
-            errorMessage = 'An error occurred';
-        }
-
-        console.log("Error message: ", errorMessage);
-        next(createError(error.response.status, errorMessage));
+        console.log("Error status: ", error.response.data.message);
+        next(createError(error.response.status, error.response.data.message));
     } else {
         console.log('Error!');
         next(createError(500, 'Internal server error'));
