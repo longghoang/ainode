b3cbb7cee428ac04c5c6699240cabd9f2c16ed09
longghoang <nguyenlonglqmb@gmail.com>
2024-08-04 23:00:07 +0700

    ok

--- src/app/utils/errorFlow.util.js
+++ src/app/utils/errorFlow.util.js
@@ -3,9 +3,13 @@ const createError = require('http-errors');
 module.exports = function errorFlow(error, next) {
     if (error.response) {
         console.log("Error status: ", error.response.status);
-        const errorMessage = error.response.data && error.response.data.message
-            ? error.response.data.message
-            : 'An error occurred';
+        console.log("Error response data: ", error.response.data);
+
+        // Extract error message from response data, with fallback options
+        const errorMessage = (error.response.data && error.response.data.message) ||
+                             (error.response.data && error.response.data.error) ||
+                             'An error occurred';
+
         console.log("Error message: ", errorMessage);
         next(createError(error.response.status, errorMessage));
     } else {
