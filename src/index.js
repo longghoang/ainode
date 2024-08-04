3048c0e987c3f571973c9d3ee1dda9d8808444b6
longghoang <nguyenlonglqmb@gmail.com>
2024-08-05 00:55:43 +0700

    ok

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv');
+const http = require('http')
 dotenv.config();
 
 const app = require('./app');
@@ -10,7 +11,9 @@ const port = process.env.PORT || 3000;
 db.connect();
 route(app);
 
+const server = http.createServer(app)
 
-app.listen(port, () => {
+
+app.listen(port, '0.0.0.0',    () => {
     console.log(`App listen on port:${port}`);
 });
