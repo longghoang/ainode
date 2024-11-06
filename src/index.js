411f48b18b67446499d261989337fcfe5c67e40d
Long Hoang <87174590+longghoang@users.noreply.github.com>
2024-11-06 08:29:08 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv');
-const http = require('http')
+const http = require('http');
 dotenv.config();
 
 const app = require('./app');
@@ -11,9 +11,22 @@ const port = process.env.PORT || 3000;
 db.connect();
 route(app);
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
+////
+app.use((req, res, next) => {
+    if (req.method === 'POST') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString(); 
+        });
+        req.on('end', () => {
+            console.log('Received POST data:', body);
+        });
+    }
+    next();
+});
 
-app.listen(port, '0.0.0.0',    () => {
-    console.log(`App listen on port:${port}`);
+server.listen(port, '0.0.0.0', () => {
+    console.log(`App listening on port: ${port}`);
 });
